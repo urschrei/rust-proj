ce778e48aa23653a5858d98cf3bea1be433bca96
Stephan Hügel <shugel@tcd.ie>
2020-09-29 17:49:15 +0100

    See whether we make it as far as the source build

--- proj-sys/build.rs
+++ proj-sys/build.rs
@@ -95,6 +95,7 @@ fn build_from_source() -> Result<std::path::PathBuf, Box<dyn std::error::Error>>
     config.define("BUILD_PROJSYNC", "OFF");
     config.define("ENABLE_CURL", "OFF");
     config.define("ENABLE_TIFF", "ON");
+    eprintln!("Beginning source build");
     let proj = config.build();
     // Tell cargo to tell rustc to link libproj, and where to find it
     // libproj will be built in $OUT_DIR/lib
